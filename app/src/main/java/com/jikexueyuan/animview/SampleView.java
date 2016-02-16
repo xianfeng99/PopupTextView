0916bad921de3cf3ede74a95c765a9ed8d2e2c21
Li xianfeng <515893218@qq.com>
2016-02-16 16:28:04 +0800

    init project

---
+++ app/src/main/java/com/jikexueyuan/animview/SampleView.java
@@ -0,0 +1,28 @@
+package com.jikexueyuan.animview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lixianfeng on 16/2/16.
+ */
+public class SampleView extends View {
+
+    public SampleView(Context context) {
+        super(context);
+    }
+
+    public SampleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawText("极客学院", 0, 200, new Paint());
+    }
+}
