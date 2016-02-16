aafbcf4cb6cc352fc016f41d7f25509ef3ee0559
Li xianfeng <515893218@qq.com>
2016-02-16 16:51:50 +0800

    draw text, geometries and bitmap

--- app/src/main/java/com/jikexueyuan/animview/SampleView.java
+++ app/src/main/java/com/jikexueyuan/animview/SampleView.java
@@ -1,7 +1,11 @@
 package com.jikexueyuan.animview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -18,9 +22,20 @@ public class SampleView extends View {
         super(context, attrs);
     }
 
+    private Paint paint = new Paint();
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        paint.setTextSize(50);
+        paint.setColor(Color.BLUE);
+        //绘制文字
+        canvas.drawText("极客学院", 0, 100, paint);
+        //绘制几何图形
+        canvas.drawCircle(50, 150, 50, paint);
+        //绘制图片
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        canvas.drawBitmap(bitmap, 0, 200, paint);
     }
+
 }
